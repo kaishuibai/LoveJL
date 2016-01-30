f38c1f0d5b01c35b45c91a7f3d3853106a01912c
clf <clf199002@163.com>
2016-01-30 13:08:16 +0800

    ceshi

---
+++ app/src/main/java/com/clf/lovejl/MainActivity.java
@@ -0,0 +1,18 @@
+package com.clf.lovejl;
+
+import android.os.Bundle;
+
+
+/**
+ * 欢迎界面
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
